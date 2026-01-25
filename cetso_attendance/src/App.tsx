c141cd291b13f9882a07e06800e8653df452e596
gela08 <angelajelly.gardan@hcdc.edu.ph>
2026-01-25 22:24:19 +0800

    logo

--- cetso_attendance/src/App.tsx
+++ cetso_attendance/src/App.tsx
@@ -9,8 +9,8 @@ import Footer from './files/components/Footer';
 
 // --- DATABASE: STUDENTS ---
 const INITIAL_STUDENTS = [
-  { id: '2023001', name: 'Juan Dela Cruz', program: 'BSIT', year: '3' },
-  { id: '2023002', name: 'Maria Santos', program: 'BSCpE', year: '2' },
+  { id: '2023001', name: 'Jerome Sagunday', program: 'BSIT', year: '3' },
+  { id: '2023002', name: 'We Miss You Jerome', program: 'BSCpE', year: '2' },
   { id: '2023003', name: 'Pedro Penduko', program: 'BSECE', year: '4' },
 ];
 
