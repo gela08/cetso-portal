71c521a83c7968147ca681fdc2f76ddc9c572d93
Angela Gardan <AngelaGardan41@gmail.com>
2026-01-28 15:54:00 +0800

    Delete src directory

--- src/files/utils/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-// src/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js'
-
-// These access your .env.local variables automatically
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
